04069256061805af19f396e5231b7ebc0e81b754
Chris Callahan <callahan.c@gmail.com>
2014-10-28 17:04:05 -0400

    fix filter again

--- 05/filteredLs.js
+++ 05/filteredLs.js
@@ -1,11 +1,9 @@
 var fs = require('fs'),
-    path = require('path');
-    dir = process.argv[2];
+    path = require('path'),
+    dir = process.argv[2],
     extension = process.argv[3];
 
 fs.readdir(dir, function (error, list) {
-  var i;
-
   if (!error) {
     list.forEach(function (file) {
       if (path.extname(file) === '.' + extension) {
