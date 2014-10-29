95ee2b060ecc98a4a0279cbfde01b266d781e53f
Chris Callahan <callahan.c@gmail.com>
2014-10-29 16:06:58 -0400

    setup http file server

---
+++ 11/httpServer.js
@@ -0,0 +1,9 @@
+var http = require('http'),
+    fs = require('fs'),
+    port = process.argv[2],
+    file = process.argv[3],
+    server = http.createServer(function (request, response) {
+      response.writeHead(200, { 'content-type': 'text/plain' });
+      
+      fs.createReadStream(file).pipe(response);
+    }).listen(port);
